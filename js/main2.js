f7b9390b1f2919618b75ba89205df784572180c6
Csaba Fazekas <csabafazekasster@gmail.com>
2014-06-21 13:24:01 +0200

    js beginning

--- js/main2.js
+++ js/main2.js
@@ -1,7 +1,8 @@
-function Q(question, answers, ga_index) {
+function Q(question, answers, ga_index, q_index) {
     this.question = question;
     this.answers = answers;
     this.ga_index = ga_index;
+    this.q_index = q_index;
     this.check = function(answer) {
         return this.answers.indexOf(answer) == this.ga_index; // looks for the index of the selected answer, 
         //and compares it to ga_index (0,1,2,3).
@@ -15,6 +16,11 @@ function Q(question, answers, ga_index) {
         for (var i = 0; i < this.answers.length; i++) { //prints the answers and puts them in .options
             $('.options-panel').append('<div class="options"><p>' + this.answers[i] + '</p></div>');
         }
+    };
+    this.q_index = function(q_index) {
+        $('.next-button').on('click', function() {
+            for(var index=0; index < this.q_index.length; index++)
+        });
     };
 }
 
@@ -32,24 +38,19 @@ function clickAnswer() {
 
 var questions = new Array();
 questions.push(// puts the questions in the new Array
-        new Q('Which one is the oldest from the following bands?', ['Pink Floyd', 'Rolling Stones', 'Guns and Roses', 'Cool and the Gang'], 1), // calls Q object
-        new Q('In which band sings Jean-Paul "Bluey"?', ['Earth Wind and Fire', 'Metallica', 'Incognito', 'The Beatles'], 2), // calls Q object
-        new Q('Which band has sold the most albums?', ['Michael Jackson', 'Bee Gees', 'Fleetwood Mac', 'Led Zeppelin'], 0), // calls Q object
-        new Q('Which one is the longest song?', ['Bohemian Rhapsody', 'Echoes', 'November Rain', 'Telegraph Road'], 0) // calls Q object
+        new Q('Which one is the oldest from the following bands?', ['Pink Floyd', 'Rolling Stones', 'Guns and Roses', 'Cool and the Gang'], 1, 1), // calls Q object
+        new Q('In which band sings Jean-Paul "Bluey"?', ['Earth Wind and Fire', 'Metallica', 'Incognito', 'The Beatles'], 2, 2), // calls Q object
+        new Q('Which band has sold the most albums?', ['Michael Jackson', 'Bee Gees', 'Fleetwood Mac', 'Led Zeppelin'], 0, 3), // calls Q object
+        new Q('Which one is the longest song?', ['Bohemian Rhapsody', 'Echoes', 'November Rain', 'Telegraph Road'], 0, 4) // calls Q object
         );
-
-    var q1 = questions[0]; //create the first question with the 0'th element of the Array
-    var q2 = questions[0];
-    var q3 = questions[0];
-    var q4 = questions[0];
 
 $(document).ready(function() {
 
+    var q1 = questions[0]; //create the first question with the 0'th element of the Array
     q1.toDOM();
     clickAnswer();
 
-    $('.options').on('click', function() { 
-        
+    $('.options').on('click', function() {
         if (q1.check($(this).text()) === true) { // q1 check method's actual parameter (the answer which was selected by user), $(this).text()= answer
             $('.answer').append('<h2>Your answer is correct!<br>Rolling Stones was established in 1962.</h2>');
         } else {
@@ -59,8 +60,6 @@ $(document).ready(function() {
     });
 
     $('.next-button').on('click', function() {
-        
-        console.log('question2');
         indicateStep();
         var q2 = questions[1]; //create the second question with the 0'th element of the Array
         q2.toDOM();
@@ -73,26 +72,8 @@ $(document).ready(function() {
             }
             $('.next-button').show();
         });
-        event.preventDefault;
     });
-   
-    
-    $('.next-button').on('click', function() {
-        console.log('question3');
-        indicateStep();
-        var q3 = questions[2]; //create the second question with the 0'th element of the Array
-        q3.toDOM();
-        clickAnswer();
-        $('.options').on('click', function() {
-            if (q3.check($(this).text()) === true) {
-                $('.answer').append('<h2>Your answer is correct!<br>Michael Jackson sold 42.4 million albűlbums from Thriller</h2>');
-            } else {
-                $('.answer').append('<h2>Your answer is incorrect!<br>Michael Jackson sold 42.4 million albűlbums from Thriller</h2>');
-            }
-            $('.next-button').show();
-        });
-    });
-    
+
 });
 
 
