eebbffeacab3503aa0341f43648642b7802ecc49
Csaba Fazekas <csabafazekasster@gmail.com>
2014-06-22 09:24:31 +0200

    js corrections

--- js/main.js
+++ js/main.js
@@ -11,17 +11,17 @@ function Q(question, answers, ga_index) {
         $('.question').html("<p>" + this.question + "</p>");
         $('.options-panel').html('');
         $('h2').html('');
-        $('.next-button').hide();
+        //$('.next-button').hide();
         for (var i = 0; i < this.answers.length; i++) { //prints the answers and puts them in .options
             $('.options-panel').append('<div class="options"><p>' + this.answers[i] + '</p></div>');
         }
     };
 }
-
 function indicateStep() {
-    $('.step-active').removeClass('step-active').prev('.step').addClass('step-active');
+    if (newId > 0) {
+        $('.step-active').removeClass('step-active').prev('.step').addClass('step-active');
+    }
 }
-
 function clickAnswer() {
     $('.options').on('click', function() {
         $('h2').hide();
@@ -32,67 +32,39 @@ function clickAnswer() {
 
 var questions = new Array();
 questions.push(// puts the questions in the new Array
-        new Q('Which one is the oldest from the following bands?', ['Pink Floyd', 'Rolling Stones', 'Guns and Roses', 'Cool and the Gang'], 1), // calls Q object
-        new Q('In which band sings Jean-Paul "Bluey"?', ['Earth Wind and Fire', 'Metallica', 'Incognito', 'The Beatles'], 2), // calls Q object
-        new Q('Which band has sold the most albums?', ['Michael Jackson', 'Bee Gees', 'Fleetwood Mac', 'Led Zeppelin'], 0), // calls Q object
-        new Q('Which one is the longest song?', ['Bohemian Rhapsody', 'Echoes', 'November Rain', 'Telegraph Road'], 0) // calls Q object
+        new Q('Which one is the oldest from the following bands?', ['Pink Floyd', 'Rolling Stones', 'Guns and Roses', 'Cool and the Gang'], 1), // calls Q object with these parameters
+        new Q('In which band sings Jean-Paul "Bluey"?', ['Earth Wind and Fire', 'Metallica', 'Incognito', 'The Beatles'], 2),
+        new Q('Which band has sold the most albums?', ['Michael Jackson', 'Bee Gees', 'Fleetwood Mac', 'Led Zeppelin'], 0),
+        new Q('Which one is the longest song?', ['Bohemian Rhapsody', 'Echoes', 'November Rain', 'Telegraph Road'], 0)
         );
 
-    var q1 = questions[0]; //create the first question with the 0'th element of the Array
-    var q2 = questions[0];
-    var q3 = questions[0];
-    var q4 = questions[0];
-
 $(document).ready(function() {
-
-    q1.toDOM();
-    clickAnswer();
-
-    $('.options').on('click', function() { 
-        
-        if (q1.check($(this).text()) === true) { // q1 check method's actual parameter (the answer which was selected by user), $(this).text()= answer
-            $('.answer').append('<h2>Your answer is correct!<br>Rolling Stones was established in 1962.</h2>');
+    var currentQuestionId = -1; // it is -1 because in the nextQuestion variable I always add 1, so the first question in the array = -1 + 1 == [0]
+    $('.next-button').on('click', function() {
+        $('.question p').hide();
+        newId = currentQuestionId + 1;
+        if (newId < questions.length) { // in default 0 < 4 -->true
+            currentQuestionId = newId; // it was -1, now it is 0
+            nextQuestion = questions[currentQuestionId];
+            nextQuestion.toDOM();
         } else {
-            $('.answer').append('<h2>Your answer is incorrect!<br>Rolling Stones was established in 1962.</h2>');
+            alert('No more questions left!');
         }
-        $('.next-button').show();
+        clickAnswer();
+        indicateStep();
+        $('.options').on('click', function() {
+            var correctness = 'incorrect'; //the answer is incorrect by default so there is no need for one more condition!
+            if (nextQuestion.check($(this).text()) === true) {
+                correctness = 'correct';
+                $('.level-eq').first().removeClass('level-eq').addClass('level-eq2').fadeIn(300);
+            }
+            var appending_text = '<h2> Your answer is ' + correctness + '!<br>';
+            //I noticed you show pretty the same string here regardless of correctness.
+            //why not to incapsulate it in your `q` objects so they just return that string?
+            $('.answer').append(appending_text);
+        });
     });
 
-    $('.next-button').on('click', function() {
-        
-        console.log('question2');
-        indicateStep();
-        var q2 = questions[1]; //create the second question with the 0'th element of the Array
-        q2.toDOM();
-        clickAnswer();
-        $('.options').on('click', function() {
-            if (q2.check($(this).text()) === true) {
-                $('.answer').append('<h2>Your answer is correct!<br>Jean-Paul "Bluey" is the singer of Incognito</h2>');
-            } else {
-                $('.answer').append('<h2>Your answer is incorrect!<br>Jean-Paul "Bluey" is the singer of Incognito</h2>');
-            }
-            $('.next-button').show();
-        });
-        event.preventDefault;
-    });
-   
-    
-    $('.next-button').on('click', function() {
-        console.log('question3');
-        indicateStep();
-        var q3 = questions[2]; //create the second question with the 0'th element of the Array
-        q3.toDOM();
-        clickAnswer();
-        $('.options').on('click', function() {
-            if (q3.check($(this).text()) === true) {
-                $('.answer').append('<h2>Your answer is correct!<br>Michael Jackson sold 42.4 million albűlbums from Thriller</h2>');
-            } else {
-                $('.answer').append('<h2>Your answer is incorrect!<br>Michael Jackson sold 42.4 million albűlbums from Thriller</h2>');
-            }
-            $('.next-button').show();
-        });
-    });
-    
 });
 
 
--- js/main2.js
+++ js/main2.js
@@ -17,6 +17,9 @@ function Q(question, answers, ga_index) {
         }
     };
 }
+function eq() {
+    
+};
 function indicateStep() {
     if (newId > 0) {
         $('.step-active').removeClass('step-active').prev('.step').addClass('step-active');
@@ -38,13 +41,17 @@ questions.push(// puts the questions in the new Array
         new Q('Which one is the longest song?', ['Bohemian Rhapsody', 'Echoes', 'November Rain', 'Telegraph Road'], 0)
         );
 
-var eq = new Array ();
-eq.push (
-        new eq($('.eq').first),
+var equalizer = new Array();
+equalizer.push(
+        new eq($('.eq').append('<img src="../img/eq1.jpg" alt="#" id="eq1" class="level-eq img-responsive" style="position: relative; bottom: -2px;" />')),
+        new eq($('.eq').append('<img src="../img/eq2.jpg" alt="#" id="eq2" class="level-eq img-responsive" style="position: relative; bottom: 1px;" />')),
+        new eq($('.eq').append('<img src="../img/eq3.jpg" alt="#" id="eq3" class="level-eq img-responsive" />')),
+        new eq($('.eq').append('<img src="../img/eq4.jpg" alt="#" id="eq4" class="level-eq img-responsive" />'))
         );
 
 $(document).ready(function() {
     var currentQuestionId = -1; // it is -1 because in the nextQuestion variable I always add 1, so the first question in the array = -1 + 1 == [0]
+    var currentEqId = -1;
     $('.next-button').on('click', function() {
         $('.question p').hide();
         newId = currentQuestionId + 1;
@@ -61,7 +68,14 @@ $(document).ready(function() {
             var correctness = 'incorrect'; //the answer is incorrect by default so there is no need for one more condition!
             if (nextQuestion.check($(this).text()) === true) {
                 correctness = 'correct';
-                $('.level-eq').next().show(300);
+                //$('.level-eq').first().fadeIn(300);
+                newEqId = currentEqId + 1;
+                if (newEqId < equalizer.length) {
+                    currentEqId = newEqId;
+                    NextEq = equalizer[currentEqId];
+                    $('.eq').append(equalizer[currentEqId]);
+                    console.log('appended');
+                }
             }
             var appending_text = '<h2> Your answer is ' + correctness + '!<br>';
             //I noticed you show pretty the same string here regardless of correctness.
